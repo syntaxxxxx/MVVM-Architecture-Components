9fae8d75278e399d9bfb9366bb573d3758b5c853
Fiqri Hafzain <fiqrihafzainislami@gmail.com>
2019-09-05 15:48:44 +0700

    provide routes movie & tv !

--- app/src/main/java/com/example/mvvm_arch_components/network/Routes.kt
+++ app/src/main/java/com/example/mvvm_arch_components/network/Routes.kt
@@ -1,7 +1,23 @@
 package com.example.mvvm_arch_components.network
 
+import com.example.mvvm_arch_components.data.model.ResponseMovies
+import com.example.mvvm_arch_components.data.model.ResponseTv
+import io.reactivex.Flowable
+import org.intellij.lang.annotations.Flow
+import retrofit2.http.GET
+
 interface Routes {
 
+    @GET("discover/movie")
+    fun getMovies() : Flowable<ResponseMovies>
 
+    @GET("discover/movi")
+    fun getErrorMovies() : Flowable<ResponseMovies>
+
+    @GET("tv/popular")
+    fun getTv() : Flowable<ResponseTv>
+
+    @GET("tv/populars")
+    fun getErrorTv() : Flowable<ResponseTv>
 
 }
