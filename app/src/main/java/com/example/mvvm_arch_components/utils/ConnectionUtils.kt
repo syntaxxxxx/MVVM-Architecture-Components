28e3dc4cc27718130ee561ed5073b45aa2bdd99b
Fiqri Hafzain <fiqrihafzainislami@gmail.com>
2019-09-10 23:00:30 +0700

    deprecated !

--- app/src/main/java/com/example/mvvm_arch_components/utils/ConnectionUtils.kt
+++ app/src/main/java/com/example/mvvm_arch_components/utils/ConnectionUtils.kt
@@ -1,14 +1,10 @@
 package com.example.mvvm_arch_components.utils
 
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkInfo
-
 object ConnectionUtils {
 
-    fun isNetworkConnected(context: Context?): Boolean {
-        val cm = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
-        return activeNetwork?.isConnectedOrConnecting == true
-    }
+//    fun isNetworkConnected(context: Context?): Boolean {
+//        val cm = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+//        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+//        return activeNetwork?.isConnectedOrConnecting == true
+//    }
 }
